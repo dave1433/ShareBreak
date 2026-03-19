dcf9cc25dfb548177bebbfebdb19a601edd0b906
David Ch <raulchuchuca951@gmail.com>
2026-03-19 15:19:42 +0100

    Entities attribute updated

--- server/Entities/Friend.cs
+++ server/Entities/Friend.cs
@@ -2,7 +2,7 @@
 
 public class Friend
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public int UserId { get; set; }
     public int FriendId { get; set; }
     public bool isBestFriend { get; set; }
