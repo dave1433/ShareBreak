6633f667a5415ee889394930050086274a37fdb4
ervinhun <127263111+ervinhun@users.noreply.github.com>
2026-03-19 16:19:21 +0100

    Add configuration helper, data seeder, JWT service, and login controller

---
+++ server/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Entities;
+using server.Util;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController(MyDbContext ctx, IConfiguration config, JwtService jwt) : ControllerBase
+{
+    [HttpPost(nameof(Login))]
+    [Produces<LoginResponseDto>]
+    public async Task<LoginResponseDto> Login([FromBody] LoginRequestDto request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Password))
+            throw new ValidationException("E-mail and password are required.");
+
+        var pepper = config["SECRET"]?.Substring(0, 16);
+        var user = await ctx.Users.FirstOrDefaultAsync(u => u.Email == request.Email && !u.IsDeleted);
+        if (user == null || !GenerateHashPass.Verify(request.Password, user.Password, pepper))
+            throw new ValidationException("Username or password is incorrect");
+        return ConvertUserToLoginResponse(user);
+    }
+
+
+    private LoginResponseDto ConvertUserToLoginResponse(User user)
+    {
+        var token = jwt.GenerateToken(user.Id.ToString(), user.Email, user.Name);
+        return new LoginResponseDto
+        {
+            Token = token,
+            Expiration = DateTime.UtcNow.AddHours(24),
+            Email = user.Email,
+            UserName = user.Name
+        };
+    }
+
+    public class LoginRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponseDto
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
--- server/Entities/User.cs
+++ server/Entities/User.cs
@@ -7,4 +7,5 @@ public class User
     public string Email { get; set; }
     public string Password { get; set; }
     public string Birthday { get; set; }
+    public bool IsDeleted { get; set; }
 }
